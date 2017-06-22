defd839ca75119b28fb0947d99abf298ac3cdc06
kiselev-webmaster <kiselev.webmaster@gmail.com>
2017-06-22 18:35:37 +0300

    Первая версия сайта

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(document).ready(function () {
+//        $(".header-nav .container").sticky({
+//            zIndex: 99999,
+//            center: true,
+//            widthFromWrapper: false
+//        });
+    });
+});
