b820b1d8e2812be9602f95454d49bbc9901ae850
kiselev-webmaster <kiselev.webmaster@gmail.com>
2017-06-23 14:14:26 +0300

    мобильная верстка

--- js/common.js
+++ js/common.js
@@ -1,9 +1,18 @@
 $(function () {
     $(document).ready(function () {
-//        $(".header-nav .container").sticky({
-//            zIndex: 99999,
-//            center: true,
-//            widthFromWrapper: false
-//        });
+        //        $(".header-nav .container").sticky({
+        //            zIndex: 99999,
+        //            center: true,
+        //            widthFromWrapper: false
+        //        });
+        $("a.skroll").click(function () {
+            $("html, body").animate({
+                scrollTop: $($(this).attr("href")).offset().top + "px"
+            }, {
+                duration: 500,
+                easing: "swing"
+            });
+            return false;
+        });
     });
 });
